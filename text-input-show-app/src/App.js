051df1a31cda7f8cecbf5f69aacb00c8449e3fac
Wiktor Sacharczuk <44320437+wiktorsachar@users.noreply.github.com>
2019-07-19 14:14:18 +0200

    fixed

--- text-input-show-app/src/App.js
+++ text-input-show-app/src/App.js
@@ -2,63 +2,6 @@
 
 import React from "react";
 
-// BINDING WAY #1 ------------------------------------------------------------------
-
-// class App extends React.Component {
-//   state = {
-//     text: ""
-//   };
-//   textTypingHandler = () => {
-//     let result = document.getElementById("textInput").value;
-//     this.setState({ text: result });
-//   };
-//   reset = () => {
-//     document.getElementById("textInput").value = "";
-//     this.setState({ text: "" });
-//   };
-//   render() {
-//     return (
-//       <>
-//         <input id="textInput" onChange={this.textTypingHandler} />
-//         <button onClick={this.reset}>Reset</button>
-//         <h1>{this.state.text}</h1>
-//       </>
-//     );
-//   }
-// }
-
-// BINDING WAY #2 ------------------------------------------------------------------
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       text: ""
-//     }
-//     this.textTypingHandler = this.textTypingHandler.bind(this);
-//     this.reset = this.reset.bind(this);
-//   }
-//   textTypingHandler() {
-//     let result = document.getElementById("textInput").value;
-//     this.setState({ text: result });
-//   };
-//   reset() {
-//     document.getElementById("textInput").value = "";
-//     this.setState({ text: "" });
-//   };
-//   render() {
-//     return (
-//       <>
-//         <input id="textInput" onChange={this.textTypingHandler} />
-//         <button onClick={this.reset}>Reset</button>
-//         <h1>{this.state.text}</h1>
-//       </>
-//     );
-//   }
-// }
-
-// BINDING WAY #3  ------------------------------------------------------------------
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -66,18 +9,17 @@ class App extends React.Component {
       text: ""
     }
   }
-  textTypingHandler() {
-    let result = document.getElementById("textInput").value;
+  textTypingHandler(e) {
+    let result = e.target.value;
     this.setState({ text: result });
   };
   reset() {
-    document.getElementById("textInput").value = "";
     this.setState({ text: "" });
   };
   render() {
     return (
       <>
-        <input id="textInput" onChange={this.textTypingHandler.bind(this)} />
+        <input value={this.state.text} onChange={this.textTypingHandler.bind(this)} />
         <button onClick={this.reset.bind(this)}>Reset</button>
         <h1>{this.state.text}</h1>
       </>
