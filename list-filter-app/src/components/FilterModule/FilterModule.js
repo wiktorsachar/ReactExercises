e1321dd6a6446189dbe02c6ff603d12307844a57
Wiktor Sacharczuk <44320437+wiktorsachar@users.noreply.github.com>
2019-09-13 21:07:28 +0200

    fixed key problem

--- list-filter-app/src/components/FilterModule/FilterModule.js
+++ list-filter-app/src/components/FilterModule/FilterModule.js
@@ -2,6 +2,7 @@ import React from "react";
 import PersonCard from "./components/PersonCard/PersonCard";
 
 const FilterModule = props =>
-  !props.display || props.display.map(e => <PersonCard data={e} />);
+  !props.display ||
+  props.display.map((e, i) => <PersonCard data={e} key={i} />);
 
 export default FilterModule;
